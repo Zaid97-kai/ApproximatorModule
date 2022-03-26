94340c7693d6a6d2e6a75367ce2d10e7ed9766f9
Заид Мингалиев <zzmingaliev@outlook.com>
2022-03-26 22:36:16 +0300

    Добавлен метод наименьших квадратов (линейная модель)

--- ImportExcel/MainWindow.xaml.cs
+++ ImportExcel/MainWindow.xaml.cs
@@ -22,30 +22,68 @@ namespace ImportExcel
     /// </summary>
     public partial class MainWindow : Window
     {
-        string[,] list = new string[50, 5];
+        private double[,] list;
+        private int _rows = 0;
+        private int _columns = 0;
+        private double _sumX = 0;
+        private double _sumXX = 0;
+        private double _sumY = 0;
+        private double _sumXY = 0;
+        private double _a = 0;
+        private double _b = 0;
         public MainWindow()
         {
             InitializeComponent();
         }
-
         private void BnOpenExcel_Click(object sender, RoutedEventArgs e)
         {
-            int n = ExportExcel();
+            ExportExcel();
             LbInputData.Items.Clear();
             string s;
-            for (int i = 0; i < n; i++) // по всем строкам
+            for (int i = 0; i < _rows; i++) // по всем строкам
             {
                 s = "";
-                for (int j = 0; j < 5; j++) //по всем колонкам
+                for (int j = 0; j < _columns; j++) //по всем колонкам
                     s += " | " + list[i, j];
                 LbInputData.Items.Add(s);
             }
+            LeastSquaresMethod();
+
+            double[] vs = new double[_rows];
+            for (int i = 0; i < _rows; i++)
+            {
+                vs[i] = _a * list[i, 0] + _b;
+                LbInputData.Items.Add(vs[i]);
+            }
         }
+        /// <summary>
+        /// Метод наименьших квадратов (линейная модель)
+        /// </summary>
+        private void LeastSquaresMethod()
+        {
+            for (int i = 0; i < _rows; i++)
+            {
+                _sumX += list[i, 0];
+                _sumXX += list[i, 0] * list[i, 0];
+                _sumY += list[i, 1];
+                _sumXY += list[i, 0] * list[i, 1];
+            }
+            LbInputData.Items.Add("SumX = " + _sumX);
+            LbInputData.Items.Add("SumXX = " + _sumXX);
+            LbInputData.Items.Add("SumY = " + _sumY);
+            LbInputData.Items.Add("SumXY = " + _sumY);
+
+            _a = Math.Sqrt(Math.Abs((_rows * _sumXY - _sumX * _sumY) / (_rows * _sumXX - _sumX * _sumX)));
+            _b = Math.Sqrt((_sumXX * _sumY - _sumX * _sumXY) / (_rows * _sumXX - _sumX * _sumX));
+            LbInputData.Items.Add("a = " + _a);
+            LbInputData.Items.Add("b = " + _b);
+        }
+
         /// <summary>
         /// Импорт данных из Excel-файла (не более 5 столбцов и любое количество строк <= 50.
         /// </summary>
         /// <returns></returns>
-        private int ExportExcel()
+        private void ExportExcel()
         {
             // Выбрать путь и имя файла в диалоговом окне
             OpenFileDialog ofd = new OpenFileDialog()
@@ -58,22 +96,24 @@ namespace ImportExcel
                 Title = "Выберите файл базы данных"
             };
             if (!(ofd.ShowDialog() == true)) // если файл БД не выбран -> Выход
-                return 0;
+                return;
+
             Excel.Application ObjWorkExcel = new Excel.Application();
             Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(ofd.FileName);
             Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1]; //получить 1-й лист
             var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);//последнюю ячейку
                                                                                                 // размеры базы
-            int lastColumn = (int)lastCell.Column;
-            int lastRow = (int)lastCell.Row;
+            _columns = (int)lastCell.Column;
+            _rows = (int)lastCell.Row;
 
-            for (int j = 0; j < 5; j++) //по всем колонкам
-                for (int i = 0; i < lastRow; i++) // по всем строкам
-                    list[i, j] = ObjWorkSheet.Cells[i + 1, j + 1].Text.ToString(); //считываем данные
+            this.list = new double[_rows, _columns];
+
+            for (int j = 0; j < _columns; j++) //по всем колонкам
+                for (int i = 0; i < _rows; i++) // по всем строкам
+                    list[i, j] = Convert.ToDouble(ObjWorkSheet.Cells[i + 1, j + 1].Text.ToString()); //считываем данные
             ObjWorkBook.Close(false, Type.Missing, Type.Missing); //закрыть не сохраняя
             ObjWorkExcel.Quit(); // выйти из Excel
             GC.Collect(); // убрать за собой
-            return lastRow;
         }
     }
 }
